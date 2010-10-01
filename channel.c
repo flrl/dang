3eb59753e762ad9cbb9659fb402ea33bd8cec6f5
flrl <flrl@users.noreply.github.com>
2010-10-02 08:31:15 +1000

    reordered functions in channel.c

--- channel.c
+++ channel.c
@@ -17,93 +17,10 @@
 #include "bits.h"
 #include "channel.h"
 
-//typedef struct channel_t {
-//    size_t m_bufsize;
-//    scalar_t *m_ringbuf;
-//    size_t m_start;
-//    size_t m_count;
-//    pthread_mutex_t m_mutex;
-//    pthread_cond_t m_has_items;
-//    pthread_cond_t m_has_space;
-//} channel_t;
-
 static const size_t MIN_BUFSIZE = 1;
 static const size_t MAX_BUFSIZE = 4096; // no particular reason
 
-static int _channel_resize_nonlocking(channel_t *self, size_t new_size) {
-    assert(self != NULL);
-    assert(new_size >= self->m_count);
-    assert(new_size != self->m_bufsize);
-    assert(new_size >= MIN_BUFSIZE);
-    assert(new_size <= MAX_BUFSIZE);
-    
-    int status = 0;    
-    scalar_t *new_ringbuf = calloc(new_size, sizeof(scalar_t));
-    if (NULL != new_ringbuf) {
-        size_t straight_count = self->m_bufsize - self->m_start > self->m_count ? self->m_count : self->m_bufsize - self->m_start;
-        size_t rotated_count = self->m_count - straight_count;
-        memcpy(&new_ringbuf[0], &self->m_ringbuf[self->m_start], straight_count * sizeof(scalar_t));
-        memcpy(&new_ringbuf[straight_count], &self->m_ringbuf[0], rotated_count * sizeof(scalar_t));
-        free(self->m_ringbuf);
-        self->m_bufsize = new_size;
-        self->m_ringbuf = new_ringbuf;
-        self->m_start = 0;
-        status = 0;
-    }
-    else {
-        status = -1;
-    }    
-
-    return status;
-}
-
-int channel_grow_buffer(channel_t *self, size_t new_size) {
-    assert(self != NULL);
-    assert(new_size >= MIN_BUFSIZE);
-    assert(new_size <= MAX_BUFSIZE);
-
-    int status = 0;
-    int post_has_space = 0;
-    
-    assert(0 == pthread_mutex_lock(&self->m_mutex));
-        if (new_size > self->m_bufsize) {
-            if (0 == (status = _channel_resize_nonlocking(self, new_size))) {
-                post_has_space = 1;            
-            }
-        }
-        else {
-            status = 0; // grow to current size or less requested: do nothing
-        }
-    pthread_mutex_unlock(&self->m_mutex);
-    
-    if (post_has_space) {
-        pthread_cond_signal(&self->m_has_space);
-    }
-    
-    return status;
-}
-
-int channel_shrink_buffer(channel_t *self, size_t new_size) {
-    assert(self != NULL);
-    assert(new_size >= MIN_BUFSIZE);
-    assert(new_size <= MAX_BUFSIZE);
-    
-    int status = 0;
-    
-    assert(0 == pthread_mutex_lock(&self->m_mutex));
-        if (new_size < self->m_bufsize) {
-            while (self->m_count > new_size) {
-                pthread_cond_wait(&self->m_has_space, &self->m_mutex);   
-            }
-            status = _channel_resize_nonlocking(self, new_size);
-        }
-        else {
-            status = 0; // shrink to current size or greater requested: do nothing
-        }
-    pthread_mutex_unlock(&self->m_mutex);
-    
-    return status;
-}
+static int _channel_resize_nonlocking(channel_t *, size_t);
 
 int channel_init(channel_t *self, size_t bufsize) {
     assert(self != NULL);
@@ -188,4 +105,77 @@ void channel_write(channel_t *self, scalar_t value) {
     pthread_cond_signal(&self->m_has_items);
 }
 
+int channel_grow_buffer(channel_t *self, size_t new_size) {
+    assert(self != NULL);
+    assert(new_size >= MIN_BUFSIZE);
+    assert(new_size <= MAX_BUFSIZE);
+    
+    int status = 0;
+    int post_has_space = 0;
+    
+    assert(0 == pthread_mutex_lock(&self->m_mutex));
+    if (new_size > self->m_bufsize) {
+        if (0 == (status = _channel_resize_nonlocking(self, new_size))) {
+            post_has_space = 1;            
+        }
+    }
+    else {
+        status = 0; // grow to current size or less requested: do nothing
+    }
+    pthread_mutex_unlock(&self->m_mutex);
+    
+    if (post_has_space) {
+        pthread_cond_signal(&self->m_has_space);
+    }
+    
+    return status;
+}
 
+int channel_shrink_buffer(channel_t *self, size_t new_size) {
+    assert(self != NULL);
+    assert(new_size >= MIN_BUFSIZE);
+    assert(new_size <= MAX_BUFSIZE);
+    
+    int status = 0;
+    
+    assert(0 == pthread_mutex_lock(&self->m_mutex));
+    if (new_size < self->m_bufsize) {
+        while (self->m_count > new_size) {
+            pthread_cond_wait(&self->m_has_space, &self->m_mutex);   
+        }
+        status = _channel_resize_nonlocking(self, new_size);
+    }
+    else {
+        status = 0; // shrink to current size or greater requested: do nothing
+    }
+    pthread_mutex_unlock(&self->m_mutex);
+    
+    return status;
+}
+
+static int _channel_resize_nonlocking(channel_t *self, size_t new_size) {
+    assert(self != NULL);
+    assert(new_size >= self->m_count);
+    assert(new_size != self->m_bufsize);
+    assert(new_size >= MIN_BUFSIZE);
+    assert(new_size <= MAX_BUFSIZE);
+    
+    int status = 0;    
+    scalar_t *new_ringbuf = calloc(new_size, sizeof(scalar_t));
+    if (NULL != new_ringbuf) {
+        size_t straight_count = self->m_bufsize - self->m_start > self->m_count ? self->m_count : self->m_bufsize - self->m_start;
+        size_t rotated_count = self->m_count - straight_count;
+        memcpy(&new_ringbuf[0], &self->m_ringbuf[self->m_start], straight_count * sizeof(scalar_t));
+        memcpy(&new_ringbuf[straight_count], &self->m_ringbuf[0], rotated_count * sizeof(scalar_t));
+        free(self->m_ringbuf);
+        self->m_bufsize = new_size;
+        self->m_ringbuf = new_ringbuf;
+        self->m_start = 0;
+        status = 0;
+    }
+    else {
+        status = -1;
+    }    
+    
+    return status;
+}
