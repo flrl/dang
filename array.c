cd0b6febec337f96c2bbd74ac5d53613bd4be0f6
flrl <flrl@users.noreply.github.com>
2010-10-23 11:31:24 +1100

    added flexible start to arrays, and array_grow_back, array_grow_front functions

--- array.c
+++ array.c
@@ -16,6 +16,7 @@
 struct array_t {
     size_t m_allocated_count;
     size_t m_count;
+    size_t m_first;
     scalar_handle_t *m_items;
 };
 
@@ -25,22 +26,16 @@ static const size_t array_grow_size = 10; // FIXME
 int array_init(array_t *self) {
     assert(self != NULL);
     
-    if (NULL != (self->m_items = calloc(array_initial_reserve_size, sizeof(*self->m_items)))) {
-        self->m_allocated_count = array_initial_reserve_size;
-        self->m_count = 0;
-        return 0;
-    }
-    else {
-        self->m_allocated_count = 0;
-        self->m_count = 0;
-        return -1;
-    }
+    memset(self, 0, sizeof(*self));
+    self->m_items = calloc(array_initial_reserve_size, sizeof(*self->m_items));
+
+    return (self->m_items != NULL ? 0 : -1);
 }
 
 int array_destroy(array_t *self) {
     assert(self != NULL);
 
-    for (size_t i = 0; i < self->m_count; i++) {
+    for (size_t i = self->m_first; i < self->m_first + self->m_count; i++) {
         scalar_release(self->m_items[i]);
     }
     free(self->m_items);
@@ -49,12 +44,13 @@ int array_destroy(array_t *self) {
     return 0;
 }
 
+// this is sort of obsolete
 int array_reserve(array_t *self, size_t new_size) {
     assert(self != NULL);
     if (self->m_allocated_count < new_size) {
         scalar_handle_t *new_items = calloc(new_size, sizeof(*new_items));
         if (NULL != new_items) {
-            memcpy(new_items, self->m_items, self->m_count * sizeof(*self->m_items));
+            memcpy(new_items, self->m_items, self->m_allocated_count * sizeof(*self->m_items));
             scalar_handle_t *tmp = self->m_items;
             self->m_items = new_items;
             free(tmp);
@@ -70,17 +66,51 @@ int array_reserve(array_t *self, size_t new_size) {
     }
 }
 
+int array_grow_back(array_t *self, size_t n) {
+    assert(self != NULL);
+    scalar_handle_t *new_items = calloc(self->m_allocated_count + n, sizeof(*new_items));
+    if (new_items != NULL) {
+        memcpy(new_items, self->m_items, self->m_allocated_count * sizeof(*self->m_items));
+        scalar_handle_t *tmp = self->m_items;
+        self->m_items = new_items;
+        self->m_allocated_count += n;
+        free(tmp);
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+int array_grow_front(array_t *self, size_t n) {
+    assert(self != NULL);
+    scalar_handle_t *new_items = calloc(self->m_allocated_count + n, sizeof(*new_items));
+    if (new_items != NULL) {
+        memcpy(&new_items[n], self->m_items, self->m_allocated_count * sizeof(*self->m_items));
+        scalar_handle_t *tmp = self->m_items;
+        self->m_items = new_items;
+        self->m_first += n;
+        self->m_allocated_count += n;
+        free(tmp);
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
 scalar_handle_t array_item_at(array_t *self, size_t index) {
     assert(self != NULL);
     assert(index < self->m_count);
     
-    return scalar_increase_refcount(self->m_items[index]);
+    return scalar_increase_refcount(self->m_items[self->m_first + index]);
 }
 
 int array_push(array_t *self, scalar_handle_t handle) {
     assert(self != NULL);
-    if (self->m_count == self->m_allocated_count) {
-        if (0 != array_reserve(self, 2 * self->m_allocated_count))  return -1;
+
+    if (self->m_first + self->m_count == self->m_allocated_count) {
+        if (0 != array_grow_back(self, self->m_count))  return -1;
     }
 
     self->m_items[self->m_count++] = scalar_increase_refcount(handle);
@@ -89,40 +119,20 @@ int array_push(array_t *self, scalar_handle_t handle) {
 
 int array_unshift(array_t *self, scalar_handle_t handle) {
     assert(self != NULL);
-    if (self->m_count == self->m_allocated_count) {
-        // FIXME this can't currently simply call reserve, cause it wants at least one 
-        // FIXME of the new items to be at the start rather than the end.
-        scalar_handle_t *tmp = self->m_items;
-        self->m_items = calloc(self->m_allocated_count + array_grow_size, sizeof(*self->m_items));
-        if (self->m_items == NULL) {
-            self->m_items = tmp;
-            return -1;
-        }
-        self->m_allocated_count += array_grow_size;
-        self->m_items[0] = scalar_increase_refcount(handle);
-        memcpy(&self->m_items[1], tmp, self->m_count * sizeof(scalar_t));
-        self->m_count++;
-        free(tmp);
-        return 0;
+    
+    if (self->m_first == 0) {
+        if (0 != array_grow_front(self, self->m_count))  return -1;
     }
-    else if (self->m_count > 0) {
-        memmove(&self->m_items[1], &self->m_items[0], self->m_count * sizeof(scalar_t));
-        self->m_items[0] = scalar_increase_refcount(handle);
-        self->m_count++;
-        return 0;
-    }
-    else {
-        self->m_items[0] = scalar_increase_refcount(handle);
-        self->m_count++;
-        return 0;
-    }
+    
+    self->m_items[--self->m_first] = scalar_increase_refcount(handle);
+    return 0;
 }
 
 scalar_handle_t array_pop(array_t *self) {
     assert(self != NULL);
 
     if (self->m_count > 0) {
-        return self->m_items[--self->m_count];
+        return self->m_items[self->m_first + --self->m_count];
     }
     else {
         return 0;
@@ -133,10 +143,8 @@ scalar_handle_t array_shift(array_t *self) {
     assert(self != NULL);
 
     if (self->m_count > 0) {
-        scalar_handle_t handle = self->m_items[0];
         --self->m_count;
-        memmove(&self->m_items[0], &self->m_items[1], self->m_count * sizeof(*self->m_items));
-        return handle;
+        return self->m_items[self->m_first++];
     }
     else {
         return 0;
