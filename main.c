cbeb8ed8271f3515670073af58c07dbb0d080368
flrl <flrl@users.noreply.github.com>
2010-10-31 10:27:55 +1100

    cleaned up and documented command line interface

--- main.c
+++ main.c
@@ -16,11 +16,11 @@
 
 #include "assembler.h"
 
-const char *options = "a:o:";
+const char *options = "aho:";
 
 int  opt_asm_flag = 0;
-char *opt_asm_file = NULL;
 char *opt_out_file = NULL;
+char *opt_in_file = NULL;
 
 void usage(void);
 char *make_filename(const char *, const char *, const char *);
@@ -32,8 +32,10 @@ int main(int argc, char *const argv[]) {
         switch (c) {
             case 'a':
                 opt_asm_flag = 1;
-                opt_asm_file = strdup(optarg);
                 break;
+            case 'h':
+                usage();
+                return 0;
             case 'o':
                 opt_out_file = strdup(optarg);
                 break;
@@ -44,32 +46,58 @@ int main(int argc, char *const argv[]) {
     }
     argc -= optind;
     argv += optind;
+    
+    if (argc) {
+        opt_in_file = strdup(argv[0]);
+    }
+    else {
+        opt_in_file = strdup("-");
+    }
 
-    if (opt_asm_flag && opt_asm_file != NULL) {
-        if (strcmp(opt_asm_file, "-") != 0)  freopen(opt_asm_file, "r", stdin);
+    if (opt_asm_flag && opt_in_file != NULL) {
+        if (strcmp(opt_in_file, "-") != 0)  freopen(opt_in_file, "r", stdin);
 
         assembler_output_t *assembly;
         if ((assembly = assemble()) != NULL) {
-            if (opt_out_file == NULL)  opt_out_file = make_filename(opt_asm_file, ".dang", ".dong");
+            if (opt_out_file == NULL)  opt_out_file = make_filename(opt_in_file, ".dang", ".dong");
             FILE *output = fopen(opt_out_file, "wb");
             write(fileno(output), "dong", 4);
             write(fileno(output), assembly, sizeof(*assembly) + assembly->m_bytecode_length);
             fclose(output);
         }
         else {
-            fprintf(stderr, "error assembling file %s\n", opt_asm_file);
+            fprintf(stderr, "error assembling file %s\n", opt_in_file);
             return 2;
         }
     }
     
-    if (opt_asm_file)  free(opt_asm_file);
-    if (opt_out_file)  free(opt_out_file);
+    if (opt_in_file)    free(opt_in_file);
+    if (opt_out_file)   free(opt_out_file);
     
     return 0;
 }
 
 void usage(void) {
-    fprintf(stderr, "%s", "Usage:  dang [-a file]\n");
+    const char *usage =
+"Usage:\n"
+"    dang [options] infile\n"
+"\n"
+"Options:\n"
+"    -a      Treat infile as assembly language input, and compile it into a bytecode file.\n"
+"            If no output filename is specified, one is constructed by either replacing a\n"
+"            \".dang\" extension on the input file with \".dong\", or if no \".dang\" extension\n"
+"            exists, by simply appending a \".dong\" extension.\n"
+"            If the input filename is not specified, or is the special value \"-\", input\n"
+"            is read from the standard input channel, and the default output filename is\n"
+"            \"a.dong\".\n"
+"\n"
+"    -h      Displays this help message\n"
+"\n"
+"    -o outfile\n"
+"            Specifies the output filename to be used by the -a, ... options.\n"
+"\n"
+    ;
+    fputs(usage, stderr);
 }
 
 char *make_filename(const char *orig, const char *orig_ext, const char *new_ext) {
