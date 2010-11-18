17f568bfa1f02016d8cbb176b7bdea7a58db1e8a
flrl <flrl@users.noreply.github.com>
2010-11-19 08:25:44 +1100

    implemented hash_list_values

--- hash.c
+++ hash.c
@@ -234,7 +234,45 @@ int hash_list_keys(hash_handle_t handle, struct scalar_t **results, size_t *coun
 
 =cut
 */
-int hash_list_values(hash_handle_t, struct scalar_t **, size_t *);
+int hash_list_values(hash_handle_t handle, struct scalar_t **results, size_t *count) {
+    assert(POOL_HANDLE_VALID(hash_t, handle));
+    assert(results != NULL);
+    assert(count != NULL);
+    
+    if (0 == POOL_LOCK(hash_t, handle)) {
+        size_t n = _hash_size_unlocked(&HASH(handle));
+        scalar_t *values;
+        int status = 0;
+
+        if (NULL != (values = calloc(n, sizeof(*values)))) {
+            size_t bucket = 0, i = 0;
+            while (i < n && bucket < HASH_BUCKETS) {
+                if (HASH(handle).m_buckets[i].m_count > 0) {
+                    hash_item_t *item = HASH(handle).m_buckets[i].m_first_item;
+                    while (i < n && item != NULL) {
+                        scalar_get_value(item->m_value, &values[i++]);
+                        item = item->m_next_item;
+                    }
+                }
+                ++bucket;
+            }
+            *results = values;
+            *count = n;
+            status = 0;
+        }
+        else {
+            debug("calloc failed: %i\n", errno);
+            status = -1;
+        }
+    
+        POOL_UNLOCK(hash_t, handle);
+        return status;
+    }
+    else {
+        debug("failed to lock hash handle %"PRIuPTR"\n", handle);
+        return -1;
+    }
+}
 
 /*
 =item hash_list_pairs()
