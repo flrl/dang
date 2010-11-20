8a33649044a9e01f6da8c67cc06beae8e70c22b3
flrl <flrl@users.noreply.github.com>
2010-11-20 14:40:28 +1100

    added a strings module

---
+++ string.c
@@ -0,0 +1,61 @@
+/*
+ *  string.c
+ *  dang
+ *
+ *  Created by Ellie on 20/11/10.
+ *  Copyright 2010 Ellie. All rights reserved.
+ *
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "debug.h"
+#include "string.h"
+
+int string_reserve(string_t **self, size_t new_size) {
+    assert(self != NULL && *self != NULL);
+    
+    if (new_size > (*self)->m_allocated_size) {
+        new_size = nextupow2(new_size + 1);
+        string_t *tmp = calloc(1, sizeof(*tmp) + new_size);
+        if (tmp) {
+            memcpy(tmp, *self, (*self)->m_length);
+            tmp->m_allocated_size = new_size;
+            tmp->m_length = (*self)->m_length;
+            free(*self);
+            *self = tmp;
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+    else {
+        return 0;
+    }
+}
+
+int string_assign(string_t **restrict self, size_t len, const char *restrict str) {
+    assert(self != NULL && *self != NULL);
+    
+    if (len > (*self)->m_allocated_size)  string_reserve(self, len);
+    
+    memcpy((*self)->m_bytes, str, len);
+    memset(&(*self)->m_bytes[len], 0, (*self)->m_allocated_size - len);
+    (*self)->m_length = len;
+    
+    return 0;
+}
+
+int string_append(string_t **restrict self, size_t len, const char *restrict str) {
+    assert(self != NULL && *self != NULL);
+    
+    if (len > (*self)->m_allocated_size)  string_reserve(self, (*self)->m_length + len);
+    
+    memcpy(&(*self)->m_bytes[(*self)->m_length], str, len);
+    (*self)->m_length += len;
+    
+    return 0;
+}
---
+++ string.h
@@ -0,0 +1,48 @@
+/*
+ *  string.h
+ *  dang
+ *
+ *  Created by Ellie on 20/11/10.
+ *  Copyright 2010 Ellie. All rights reserved.
+ *
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+
+typedef struct string_t {
+    size_t m_allocated_size;
+    size_t m_length;
+    char m_bytes[];
+} string_t;
+
+static inline string_t *string_alloc(size_t initial_length, const char *initial_value) {
+    size_t size = nextupow2(initial_length + 1);
+    string_t *self = calloc(1, sizeof(*self) + size);
+    if (self) {
+        self->m_allocated_size = size;
+        if (initial_value) {
+            memcpy(self->m_bytes, initial_value, initial_length);
+            self->m_length = initial_length;
+        }
+    }
+    return self;
+}
+
+static inline void string_free(string_t *self) { free(self); }
+
+int string_reserve(string_t **, size_t);
+int string_assign(string_t **restrict, size_t, const char *restrict);
+
+int string_append(string_t **restrict, size_t, const char *restrict);
+int string_prepend(string_t **restrict, size_t, const char *restrict);
+
+static inline size_t string_length(const string_t *self) { return self->m_length; }
+static inline const char *string_cstr(const string_t *self) { return self->m_bytes; }
+
+#endif
